e984d7178a3f5c6e3734f03b6bc2cbc1dfbf0751
joaogabrielmp <joaogabrielma@gmail.com>
2020-06-25 13:36:39 -0300

    up

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -9,7 +9,7 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-class AppointmentsRepository implements IAppointmentsRepository {
+class FakeAppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async create({
@@ -68,4 +68,4 @@ class AppointmentsRepository implements IAppointmentsRepository {
   }
 }
 
-export default AppointmentsRepository;
+export default FakeAppointmentsRepository;
