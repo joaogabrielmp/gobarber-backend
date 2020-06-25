b0c84ef618941904321ad528de0a6d3c72aa3d9f
joaogabrielmp <joaogabrielma@gmail.com>
2020-06-25 17:54:20 -0300

    up

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import passwordsRouter from '@modules/users/infra/http/routes/passwords.routes';
-// import profilesRouter from '@modules/users/infra/http/routes/profiles.routes';
+import profilesRouter from '@modules/users/infra/http/routes/profiles.routes';
 // import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/user.routes';
@@ -11,7 +11,7 @@ const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/password', passwordsRouter);
-// routes.use('/profile', profilesRouter);
+routes.use('/profile', profilesRouter);
 // routes.use('/providers', providersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
