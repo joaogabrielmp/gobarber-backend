733abe3074a4b68b35b903b2bfe74c76bc200612
joaogabrielmp <joaogabrielma@gmail.com>
2020-06-25 17:44:55 -0300

    up

---
+++ src/modules/users/infra/http/routes/profiles.routes.ts
@@ -0,0 +1,27 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+
+import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+const profilesRouter = Router();
+const profileController = new ProfileController();
+
+profilesRouter.use(ensureAuthenticated);
+
+profilesRouter.get('/', profileController.show);
+profilesRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update,
+);
+
+export default profilesRouter;
