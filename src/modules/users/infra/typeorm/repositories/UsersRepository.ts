979b58c969bc77f59dc2c73628da53d33067796b
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-06 13:51:47 -0300

    up

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -16,6 +16,8 @@ class UsersRepository implements IUsersRepository {
   public async create(userData: ICreateUserDTO): Promise<User> {
     const user = this.ormRepository.create(userData);
 
+    await this.ormRepository.save(user);
+
     return user;
   }
 
